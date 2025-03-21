7c58af6b591a7db8e93439d43f37c7ced25de6c2
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2025-03-22 02:56:56 +0800

    some updates

--- database/migrations/2024_12_13_061630_create_customers_table.php
+++ database/migrations/2024_12_13_061630_create_customers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('username')->unique()->nullable();
-            $table->enum('gender', ['male', 'female', 'other']);
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
             $table->date('birthday')->nullable();
             $table->string('phone')->nullable();
             $table->string('email')->unique();
