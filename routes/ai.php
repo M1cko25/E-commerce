d6804a09d03de8fd9748935d8e8dc8b698ad6395
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2025-03-22 02:24:38 +0800

    file removed

--- routes/ai.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AiController;
-
-Route::post('/ai/process', [AIController::class, 'processPrompt']);
-// Route::post('/ai/getProjectData', [AIController::class, 'getProjectData']);
-
