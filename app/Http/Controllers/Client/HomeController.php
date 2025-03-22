43eaf265748175a06e22c37853a90c2c020d63e1
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2025-03-22 11:30:49 +0800

    Update HomeController.php

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
         $brands = Brand::all();
         $landingContents = LandingContent::where('is_active', 1)->get();
         $exploreProducts = Product::with(['category', 'brand', 'specifications'])
-            ->take(7)
+            ->take(0)
             ->get()
             ->map(function ($product) {
                 return [
@@ -33,10 +33,10 @@ class HomeController extends Controller
                     'specifications' => $product->specifications
                 ];
             })
-            ->random(7);
+            ->random(0);
         $latestProducts = Product::with(['category', 'brand', 'specifications'])
             ->latest()
-            ->take(7)
+            ->take(0)
             ->get()
             ->map(function ($product) {
                 return [
