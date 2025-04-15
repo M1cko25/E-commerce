01612d924eef36839f9d7cfd46f0778f6ecf2c70
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2025-04-15 23:02:44 +0800

    Update ProductSeeder.php

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,12 +5,26 @@ namespace Database\Seeders;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
+use App\Models\Brand;
 
 class ProductSeeder extends Seeder
 {
     public function run()
     {
         // Generate 20 random products
-        Product::factory(10)->create();
+        $products = [
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'brand_id' => Brand::inRandomOrder()->first()->id,
+            'name' => 'temporary name',
+            'slug' => 'temporary-slug'. rand(1, 1000),
+            'sku' => 'TEMporarySKU'. rand(1, 1000),
+            'description' => "description",
+            'price' => rand(100, 5000),
+            'stock' => rand(0, 100),
+            'product_images' => "images",
+        ];
+        for ($i = 0; $i < 10; $i++) {
+            Product::create($products);
+        }
     }
 }
