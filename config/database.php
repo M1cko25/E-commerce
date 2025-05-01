19971d94220e0052740e7e4979d4aaf7ce53af52
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2025-05-02 00:24:29 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -84,12 +84,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DB_URL', 'postgresql://postgres:cQJcNGeqhRzmmTlvnZrEGFqOBtdYGLBM@postgres.railway.internal:5432/railway'),
+            'host' => env('DB_HOST', 'postgres.railway.internal'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'railway'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'cQJcNGeqhRzmmTlvnZrEGFqOBtdYGLBM'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
