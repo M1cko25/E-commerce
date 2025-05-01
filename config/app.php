853b9cb64bcb690298fe6ff2c35a1da1361acb27
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2025-05-02 01:22:17 +0800

    some adjustment

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://e-commerce-production-274f.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
